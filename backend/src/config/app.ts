3ec1a58044e32d3adadc15afa899744338aeb2a9
ArtemSmotritel <artemsmotritel@gmail.com>
2026-01-15 00:14:01 +0200

    refactor(backend): extract `setSession` middleware

--- backend/src/config/app.ts
+++ backend/src/config/app.ts
@@ -4,7 +4,7 @@ import { HTTPException } from "hono/http-exception";
 import { logger } from "hono/logger";
 import { auth } from "utils/auth";
 import { log } from "utils/logger";
-import { setUserRole } from "../middlewares/auth";
+import { setSession, setUserRole } from "../middlewares/auth";
 
 export const app = new Hono<{
     Variables: {
@@ -61,18 +61,6 @@ app.notFound((c) => {
 
 app.on(["POST", "GET"], "/api/auth/*", (c) => auth.handler(c.req.raw));
 
-app.use("*", async (c, next) => {
-    const session = await auth.api.getSession({ headers: c.req.raw.headers });
-    if (!session) {
-        c.set("user", null);
-        c.set("session", null);
-        await next();
-        return;
-    }
-
-    c.set("user", session.user);
-    c.set("session", session.session);
-    await next();
-});
+app.use("*", setSession);
 
 app.use(setUserRole);
